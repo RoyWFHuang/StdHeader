87270868e98c76e9821b430f416803e3cdaa608c
royHuang <roywfhuang@gmail.com>
2019-10-13 11:23:41 +0800

    Add base macro

---
+++ define.h
@@ -0,0 +1,60 @@
+#ifndef __define_H__
+#define __define_H__
+#include <sys/syslog.h>
+#include <pthread.h>
+#ifdef __GNUC__
+#define likely(x)       __builtin_expect(!!(x), 1)
+#define unlikely(x)     __builtin_expect(!!(x), 0)
+#else
+#define likely(x)       (x)
+#define unlikely(x)     (x)
+#endif
+
+
+#ifdef ERROR_MSG_MODE
+#   ifdef SYSLOG
+#       define PRINTERR_OUTFILE stderr
+#   endif /* PRINTERR_OUTFILE */
+#   ifndef PRINT_ERR
+#       define PRINT_ERR log_err
+#   endif /* PRINT_ERR */
+#else
+#   ifndef PRINT_ERR
+#       define PRINT_ERR(fmt, str...)
+#   endif /* PRINT_ERR */
+#endif /* ERROR_MSG_MODE */
+
+#ifdef DEBUG_MODE
+#   ifndef SYSLOG
+#       define PRINTMSG_OUTFILE stdout
+#   endif /* PRINTMSG_OUTFILE */
+#   ifndef PRINT_DBG
+#       define PRINT_DBG log_debug
+#   endif /* PRINT_DBG */
+#else
+#   ifndef PRINT_DBG
+#       define PRINT_DBG(fmt, str...)
+#   endif /* PRINT_DBG */
+#endif /* DEBUG_MODE */
+
+#ifndef SYSLOG
+#   define lkmlog(level, mode, fmt, ...) \
+    fprintf(level, "[%lu] [%5.5s] - %16.16s(%4d) - %16.16s: " fmt , \
+        pthread_self(), mode, __FILE__, __LINE__, __func__, ##__VA_ARGS__);
+#define log_debug(fmt, ...) \
+    lkmlog(PRINTMSG_OUTFILE, "DEBUG", fmt, ##__VA_ARGS__);
+#define log_err(fmt, ...) \
+    lkmlog(PRINTERR_OUTFILE, "ERROR", fmt, ##__VA_ARGS__);
+#else
+#   define lkmlog(level, mode, fmt, ...) \
+    syslog(level, "[%lu] [%5.5s] - %16.16s(%4d) - %16.16s: " fmt , \
+        pthread_self(), mode, __FILE__, __LINE__, __func__, ##__VA_ARGS__);
+#define log_debug(fmt, ...) \
+    lkmlog(LOG_DEBUG, "DEBUG", fmt, ##__VA_ARGS__);
+#define log_err(fmt, ...) \
+    lkmlog(LOG_ERR, "ERROR", fmt, ##__VA_ARGS__);
+#endif
+
+
+
+#endif
